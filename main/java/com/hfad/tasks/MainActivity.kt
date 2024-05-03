3b4f5f3c0fcb4114830877838d710f73ac651423
gabbypierce <144390401+gabbypierce@users.noreply.github.com>
2024-05-02 23:40:11 -0400

    Add files via upload

---
+++ main/java/com/hfad/tasks/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.hfad.tasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//import edu.quinnipiac.ser210.ls06_network_nav.R
+import edu.quinnipiac.ser210.tasks.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ main/java/com/hfad/tasks/TasksFragment.kt
@@ -0,0 +1,59 @@
+package com.hfad.tasks
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+//import com.hfad.tasks.databinding.FragmentTasksBinding
+//import edu.quinnipiac.ser210.tasks.TaskDatabase
+//import edu.quinnipiac.ser210.tasks.TasksViewModel
+import edu.quinnipiac.ser210.tasks.databinding.FragmentTasksBinding
+import androidx.navigation.fragment.findNavController
+
+class TasksFragment : Fragment() {
+    private var _binding: FragmentTasksBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTasksBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val application = requireNotNull(this.activity).application
+        val dao = TaskDatabase.getInstance(application).taskDAO
+        val viewModelFactory = TasksViewModelFactory(dao)
+        val viewModel = ViewModelProvider(
+            this, viewModelFactory).get(TasksViewModel::class.java)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        val adapter = TaskItemAdapter{ taskId ->
+            viewModel.onTaskClicked(taskId)
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
+
+        viewModel.navigateToTask.observe(viewLifecycleOwner, Observer { taskId ->
+            taskId?.let {
+                val action = TasksFragmentDirections
+                    .actionTasksFragmentToEditTaskFragment(taskId)
+                this.findNavController().navigate(action)
+                viewModel.onTaskNavigated()
+            }
+        })
+
+        return view
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
